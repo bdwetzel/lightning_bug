f006133c7bb734edac566d1b5348922deede71f6
Brian Wetzel <bdwetzel83@gmail.com>
2011-11-29 22:37:48 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LightningBug::Application.config.secret_token = 'c022b4a12364a3f9ba9b7e0932eeb761d6c191434e4a1d704d88f885f1aaf76b096f39b549afb16716a28c1d1ace3b642770a0cdd87a879dbe199a4b4ec5057d'
